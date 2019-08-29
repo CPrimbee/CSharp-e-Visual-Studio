c4f89f4fe029b3b1bff258914601fa1c75529af9
Cristiano Gonçalves <cprimbee@gmail.com>
2019-08-29 13:47:04 -0300

    Resto da divisão

---
+++ SolucaoCapitulo1/RestoDaDivisao/FormRestoDeDivisao.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace RestoDaDivisao
+{
+    partial class frmRestoDeDivisao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDividendo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDivisor = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtRestoDaDivisao = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dividendo:";
+            // 
+            // txtDividendo
+            // 
+            this.txtDividendo.Location = new System.Drawing.Point(66, 9);
+            this.txtDividendo.Name = "txtDividendo";
+            this.txtDividendo.Size = new System.Drawing.Size(100, 20);
+            this.txtDividendo.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(172, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Divisor:";
+            // 
+            // txtDivisor
+            // 
+            this.txtDivisor.Location = new System.Drawing.Point(212, 9);
+            this.txtDivisor.Name = "txtDivisor";
+            this.txtDivisor.Size = new System.Drawing.Size(100, 20);
+            this.txtDivisor.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(176, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Resto:";
+            // 
+            // txtRestoDaDivisao
+            // 
+            this.txtRestoDaDivisao.Enabled = false;
+            this.txtRestoDaDivisao.Location = new System.Drawing.Point(212, 35);
+            this.txtRestoDaDivisao.Name = "txtRestoDaDivisao";
+            this.txtRestoDaDivisao.ReadOnly = true;
+            this.txtRestoDaDivisao.Size = new System.Drawing.Size(100, 20);
+            this.txtRestoDaDivisao.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Realizar Divisão";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // frmRestoDeDivisao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 61);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtRestoDaDivisao);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDivisor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDividendo);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmRestoDeDivisao";
+            this.Text = "Resto de uma divisão";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDividendo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDivisor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtRestoDaDivisao;
+        private System.Windows.Forms.Button button1;
+    }
+}
